d99cb964dfd8063071c58e7ad705226b76c95357
hodumaitran <149867559+hodumaitran@users.noreply.github.com>
2025-01-22 02:15:43 +0700

    FE-Banner

--- FE/src/layouts/MainLayout.jsx
+++ FE/src/layouts/MainLayout.jsx
@@ -1,12 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "../components/client/header";
-import { Footer } from "../components/Client/footer"; 
+import { Footer } from "../components/Client/footer";
+import Banner from "@/components/Client/Banner";
 
 export const MainLayout = () => {
+    const location = useLocation();
+
     return (
         <div>
             <Header />
             <main className="flex-1 bg-[#F8F9FD] text-black">
+                <div className="w-full max-w-[1200px] mx-auto">
+                    {location.pathname === "/" && <Banner />}
+                </div>
                 <div className="w-full max-w-[1200px] mx-auto py-4 px-4 lg:px-0">
                     <Outlet />
                 </div>
